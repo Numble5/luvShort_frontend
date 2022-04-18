1fc953cc1887e8b21c0871040bda0eb609199c96
urther <alswlkku@ajou.ac.kr>
2022-04-19 03:15:27 +0900

    design : #19 add navigaion component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 
 import { theme, GlobalStyle } from "@styles";
+import Routers from "@routes";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider theme={theme}>
-        <App />
+        <Routers />
         <GlobalStyle />
       </ThemeProvider>
     </BrowserRouter>
