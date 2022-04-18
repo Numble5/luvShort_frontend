9a6ecb63bb4be141276cb8c700e09443d01085bc
savannah030 <savannah030@naver.com>
2022-04-18 21:24:45 +0900

    feat : #9 User 엔티티 빌더패턴 추가

--- backend/src/main/java/com/example/backend/domain/user/User.java
+++ backend/src/main/java/com/example/backend/domain/user/User.java
@@ -5,6 +5,7 @@ import com.example.backend.domain.likes.Likes;
 import com.example.backend.domain.user.embedded.UserInfo;
 import com.example.backend.domain.user.enums.UserType;
 import com.example.backend.domain.video.Video;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -40,6 +41,15 @@ public class User extends BaseEntity {
 
     @OneToMany(mappedBy = "uploader", fetch = FetchType.LAZY)
     private List<Video> myVideos = new LinkedList<>();
+
+    @Builder
+    public User(String email, String nickname, UserType userType, UserInfo userInfo, Profile profile){
+        this.email = email;
+        this.nickname = nickname;
+        this.userType = userType;
+        this.userInfo = userInfo;
+        this.profile = profile;
+    }
 
     public void addMyVideo(Video myVideo){
         myVideo.setUser(this);
