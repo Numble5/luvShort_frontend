d4e5b8d1df25698bf808286581b36a804be9ea80
yaulhae <gilga4@naver.com>
2022-04-19 16:21:04 +0900

    design : #11 login page markup

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 import Navigator from "@components/navigator";
+import Login from "@/pages/login";
+import OnBoarding from "@/pages/onBoarding";
 
 const Routers = (props) => {
   return (
     <>
-      <Navigator />
-      <Routes>{/* <Route to="" element={}/> */}</Routes>
+      <Routes>
+        {/* <Route to="" element={}/> */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<OnBoarding />} />
+      </Routes>
+      {/*<Navigator />*/}
     </>
   );
 };
