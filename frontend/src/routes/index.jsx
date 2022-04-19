fae51bb6c795a9ed54e9c7ef59f5921b1d485632
urther <alswlkku@ajou.ac.kr>
2022-04-19 16:50:57 +0900

    feat : #25 add routes main page

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 
 import Navigator from "@components/navigator";
+import Main from "@pages/main";
 
 const Routers = (props) => {
   return (
     <>
       <Navigator />
-      <Routes>{/* <Route to="" element={}/> */}</Routes>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
     </>
   );
 };
