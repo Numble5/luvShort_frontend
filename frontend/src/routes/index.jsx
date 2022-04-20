5bd9d86a415283c5123eaf7a9da9a5b05ca9b479
yaulhae <gilga4@naver.com>
2022-04-20 09:29:59 +0900

    feat : #30 add routes step1 page

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -4,14 +4,16 @@ import { Route, Routes } from "react-router-dom";
 import Navigator from "@components/navigator";
 import Login from "@/pages/login";
 import OnBoarding from "@/pages/onBoarding";
+import Step1Page from "@/pages/step/Step1Page";
 
 const Routers = (props) => {
   return (
     <>
       <Routes>
         {/* <Route to="" element={}/> */}
-        <Route path="/login" element={<Login />} />
         <Route path="/" element={<OnBoarding />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/step1" element={<Step1Page />} />
       </Routes>
       {/*<Navigator />*/}
     </>
