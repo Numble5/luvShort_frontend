bab6b4e5ef049a1ca7e82223feb086cfc41376c2
yaulhae <gilga4@naver.com>
2022-04-20 20:22:52 +0900

    feat : #35 add routes Step2 page

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -5,6 +5,7 @@ import Login from "@/pages/login";
 import OnBoarding from "@/pages/onBoarding";
 import Step1Page from "@/pages/step/Step1Page";
 import Main from "@pages/main";
+import Step2Page from "@/pages/step/Step2Page";
 
 const Routers = (props) => {
   return (
@@ -12,6 +13,7 @@ const Routers = (props) => {
       <Routes>
         {/* <Route to="" element={}/> */}
         <Route path="/step1" element={<Step1Page />} />
+        <Route path="/step2" element={<Step2Page />} />
         <Route path="/on" element={<OnBoarding />} />
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Main />} />
