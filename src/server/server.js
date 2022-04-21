572a47353cbb9c2b58d2595ac2ba0f51f6af6c2d
urther <alswlkku@ajou.ac.kr>
2022-04-21 21:25:14 +0900

    remove : remove server file

--- src/server/server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const path = require("path");
-const app = express();
-
-const http = require("http").createServer(app);
-
-http.listen(8080, () => {
-  console.log("listen ");
-});
