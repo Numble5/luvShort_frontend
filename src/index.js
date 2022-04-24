c5674254e92f806fe59b38f2e33b4aa949f592fd
urther <alswlkku@ajou.ac.kr>
2022-04-24 20:58:04 +0900

    rename : move file directory

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
 
+import { ThemeProvider } from "styled-components";
+import { Provider } from "react-redux";
 import { theme, GlobalStyle } from "@styles";
 import Routers from "@routes";
-import Template from "./components/common/Template";
-import { Provider } from "react-redux";
-import store from "./store/configureStore";
+import store from "@redux/store/configureStore";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
