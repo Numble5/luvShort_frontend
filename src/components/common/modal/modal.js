9b4ee9e25adb7c4bbb010b7e341abf32ff6ff71b
urther <alswlkku@ajou.ac.kr>
2022-04-25 14:35:13 +0900

    feat : #25 add image carousel feature

--- src/components/common/modal/modal.js
+++ src/components/common/modal/modal.js
@@ -1,36 +1,34 @@
 import React from "react";
 import styled from "styled-components";
+import Carousel from "../../carousel/carousel";
 
 const MainLoginModal = () => {
+  const items = [
+    {
+      idx: "carousel1",
+      title: "자연스러운 매칭 서비스",
+      content:
+        "조회수, 좋아요수는 필요 없어요! 그냥 서로의 영상에 하트를 누르면 자연스럽게 매칭돼요.",
+    },
+    {
+      idx: "carousel2",
+      title: "관심사 기반의 추천",
+      content:
+        "나와 관심사/취미가 비슷한 사람을 찾고 싶으신가요? 회원님의 관심사가 반영된 영상을 추천해드려요.",
+    },
+    {
+      idx: "carousel3",
+      title: "차별화된 소개방식",
+      content:
+        "외모, 직업, 인기등급 다 좋지만, 좀 더 다양한 매력을 짧은 영상에 담아 나를 소개해보세요!",
+    },
+  ];
+
   return (
     <StyledModal>
-      <ul>
-        <li>
-          <h2>자연스러운 매칭 서비스</h2>
-          <div>
-            조회수, 좋아요수는 필요 없어요!
-            <br />
-            그냥 서로의 영상에 하트를 누르면 자연스럽게 매칭돼요.
-          </div>
-        </li>
-        {/* <li>
-          <h2>관심사 기반의 추천</h2>
-          <div>
-            나와 관심사/취미가 비슷한 사람을 찾고 싶으신가요?
-            <br />
-            회원님의 관심사가 반영된 영상을 추천해드려요.
-          </div>
-        </li>
-        <li>
-          <h2>차별화된 소개방식</h2>
-          <div>
-            외모, 직업, 인기등급 다 좋지만, 좀 더 다양한 매력을
-            <br />
-            짧은 영상에 담아 나를 소개해보세요!
-          </div>
-        </li> */}
-      </ul>
-      <div></div>
+      <div className="loginModal_wrapper">
+        <Carousel items={items} />
+      </div>
       <button>로그인하고 시작하기</button>
     </StyledModal>
   );
@@ -40,13 +38,19 @@ export default MainLoginModal;
 
 const StyledModal = styled.div`
   position: absolute;
+  width: 85%;
   z-index: 2;
-  top: 30%;
+  top: 20%;
   left: 50%;
   transform: translateX(-50%);
   background-color: #ffffff;
   border-radius: 10px;
   overflow: hidden;
+
+  .loginModal_wrapper {
+    margin: 13px;
+    margin-bottom: 25px;
+  }
 
   > button {
     pointer: cursor;
@@ -57,5 +61,17 @@ const StyledModal = styled.div`
     width: 100%;
     font-size: 17px;
     height: 50px;
+  }
+
+  .slick-dots {
+    bottom: -15px;
+  }
+
+  .slick-dots li.slick-active button:before {
+    color: #5dccc6;
+  }
+
+  .slick-dots li {
+    margin: 0;
   }
 `;
