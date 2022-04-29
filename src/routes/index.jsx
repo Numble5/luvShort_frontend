d88d99b97df61ddfc306d1949516d68a918e3e66
urther <alswlkku@ajou.ac.kr>
2022-04-30 01:08:22 +0900

    feat : #75 add detail page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,29 +1,29 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
-import Login from "@/pages/login";
-import OnBoarding from "@/pages/onBoarding";
-import Step1Page from "@/pages/step/Step1Page";
+import Login from "@pages/login";
+import Step1Page from "@pages/step/Step1Page";
 import Main from "@pages/main";
-import Step2Page from "@/pages/step/Step2Page";
+import Step2Page from "@pages/step/Step2Page";
 import RegisterSuccessPage from "@/pages/step/RegisterSuccessPage";
-import Interests from "@/pages/Interests/noInterests";
-import LoginCallbackPage from "@/pages/login/LoginCallbackPage";
-import FileUploadPage from "@/pages/file-upload/FileUploadPage";
+import Interests from "@pages/Interests/noInterests";
+import LoginCallbackPage from "@pages/login/LoginCallbackPage";
+import FileUploadPage from "@pages/file-upload/FileUploadPage";
+import Detail from "@pages/detail";
 
 const Routers = (props) => {
   return (
     <>
       <Routes>
         {/* <Route to="" element={}/> */}
+        <Route path="/" element={<Main />} />
+        <Route path="/:id" element={<Detail />} />
         <Route path="/step1" element={<Step1Page />} />
         <Route path="/step2" element={<Step2Page />} />
         <Route path="/register-success" element={<RegisterSuccessPage />} />
-        <Route path="/on" element={<OnBoarding />} />
         <Route path="/login" element={<Login />} />
         <Route path="/naver/auth/callback" element={<LoginCallbackPage />} />
-        <Route path="/" element={<Main />} />
-        <Route path="/interest" element={<Interests/>} />
+        <Route path="/interest" element={<Interests />} />
         <Route path="/file-upload" element={<FileUploadPage />} />
       </Routes>
     </>
