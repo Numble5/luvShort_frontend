64a447ac91ec70b5cd5cf0a1a41135498499897a
urther <alswlkku@ajou.ac.kr>
2022-05-04 00:30:07 +0900

    feat: #75 add detail page data fetch feature

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,14 +1,18 @@
 import request from "@/api/request";
 import Navigator from "@components/navigator";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router";
+
+import { FixedUploadBtn } from "@components/common/button";
 
 const Detail = (props) => {
   const [, pathname] = useLocation().pathname.split("/");
+  const [videoInfo, setVideoInto] = useState(null);
 
   const fetchData = async () => {
     try {
-      const { data } = await request(`/api/videos/${pathname}`, "get");
+      const data = await request(`/api/videos/${pathname}`, "get");
+      setVideoInto(data);
     } catch (e) {
       console.log(e);
     }
@@ -17,10 +21,33 @@ const Detail = (props) => {
   useEffect(() => {
     fetchData();
   }, []);
+
   return (
-    <div>
+    <section>
       <Navigator />
-    </div>
+
+      <div>
+        <div>
+          <div>
+            <img src={videoInfo?.profileImgUrl} alt="프로필 이미지" />
+            <div>
+              <span>{videoInfo?.nickname}</span>
+            </div>
+          </div>
+          <div></div>
+        </div>
+        <div>
+          <div>{videoInfo?.content}</div>
+          <div>{videoInfo?.createdDate}</div>
+        </div>
+        <div>
+          <video>
+            <source src={videoInfo?.videoUrl} />
+          </video>
+        </div>
+      </div>
+      <FixedUploadBtn />
+    </section>
   );
 };
 
