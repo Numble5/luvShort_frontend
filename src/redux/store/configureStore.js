b32f3353d46369856664ddadcf1031d944025984
urther <alswlkku@ajou.ac.kr>
2022-05-04 02:22:43 +0900

    chore : #87 install redux-persist

--- src/redux/store/configureStore.js
+++ src/redux/store/configureStore.js
@@ -1,12 +1,28 @@
-import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "@redux/reducers/user";
-import navigatorReducer from "@redux/reducers/navigator";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer } from "redux-persist";
+import thunk from "redux-thunk";
+import storage from "redux-persist/lib/storage";
+
+import user from "@redux/reducers/user";
+import navigator from "@redux/reducers/navigator";
+
+const reducers = combineReducers({
+  user,
+  navigator,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  // whitelist: [],
+  // whitelist 이용시 그것만 제외됨
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 const store = configureStore({
-  reducer: {
-    user: userReducer,
-    navigator: navigatorReducer,
-  },
+  reducer: persistedReducer,
+  middleware: [thunk],
 });
 
 export default store;
