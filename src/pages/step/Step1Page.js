da25214569738eb5871d6965c718711c8fcfe0e5
yaulhae <gilga4@naver.com>
2022-05-08 20:06:47 +0900

    modify step1Page profileForm Componentization

--- src/pages/step/Step1Page.js
+++ src/pages/step/Step1Page.js
@@ -2,25 +2,8 @@ import styled from "styled-components";
 import React, { useCallback, useState } from "react";
 import LeftArrow from "@/static/step/Vector 3.svg";
 import RightArrow from "@/static/step/Vector 2.svg";
-import NicknameCheck from "@/static/step/alert_circle.svg";
-import GenderGrayCheckButton from "@/static/step/Group 39531.svg";
-import GenderGreenCheckButton from "@/pages/step/assets-step1/Group 39571.svg";
-import NicknameError from "@pages/step/assets-step1/Group 39570.svg";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  changeBirthday,
-  changeDistrict,
-  changeGender,
-  changeNickname,
-  changeCity,
-} from "@redux/reducers/user";
 import Modal from "@components/step1/modal";
-import {
-  changeBirtdayError,
-  nicknameCheck,
-  setNicknameCheckNull,
-} from "@/redux/reducers/user";
-import { useNavigate } from "react-router";
+import ProfileForm from "@/components/step1/ProfileForm";
 
 const Step1PageBlock = styled.div`
   padding: 53px 30px 0 30px;
@@ -217,75 +200,7 @@ const Step1PageBlock = styled.div`
 `;
 
 const Step1Page = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const nickname = useSelector(({ user }) => user.nickname);
-  const birthday = useSelector(({ user }) => user.birthday);
-  const gender = useSelector(({ user }) => user.gender);
-  const nicknameCheckError = useSelector(({ user }) => user.nicknameCheckError);
-  const birthdayError = useSelector(({ user }) => user.birthdayError);
   const [isModal, setIsModal] = useState(false);
-
-  const onChangeNickname = useCallback((e) => {
-    dispatch(changeNickname(e.target.value));
-  }, []);
-
-  const onChangeBirthday = useCallback((e) => {
-    const onlyNumber = e.target.value.replace(/[^0-9]/g, "");
-    dispatch(changeBirthday(onlyNumber));
-    if (e.target.value.length === 8) {
-      birthdayCheck(e.target.value);
-    }
-  }, []);
-
-  const birthdayCheck = (birth) => {
-    var format =
-      /^(19[0-9][0-9]|20\d{2})(0[0-9]|1[0-2])(0[1-9]|[1-2][0-9]|3[0-1])$/;
-    if (
-      Number(birth.substring(0, 2)) !== 19 &&
-      Number("20" + birth.substring(2, 4)) > Number(new Date().getFullYear())
-    ) {
-      return dispatch(changeBirtdayError(true));
-    }
-    if (
-      Number(birth.substring(0, 2)) !== 19 &&
-      Number(birth.substring(4, 6)) > new Date().getMonth() + 1
-    ) {
-      return dispatch(changeBirtdayError(true));
-    }
-    if (
-      Number(birth.substring(0, 2)) !== 19 &&
-      Number(birth.substring(4, 6)) === new Date().getMonth() + 1 &&
-      Number(birth.substring(6, 8)) > new Date().getDay() + 1
-    ) {
-      return dispatch(changeBirtdayError(true));
-    }
-    if (Number(birth.substring(4, 6)) === 0) {
-      return dispatch(changeBirtdayError(true));
-    }
-    if (format.test(birth)) {
-      dispatch(changeBirtdayError(false));
-    } else {
-      dispatch(changeBirtdayError(true));
-    }
-  };
-
-  const checkGender = (gender) => {
-    dispatch(changeGender(gender));
-  };
-
-  const onChangeCity = useCallback((e) => {
-    dispatch(changeCity(e.target.value));
-  }, []);
-
-  const onChangeDistrict = (e) => {
-    dispatch(changeDistrict(e.target.value));
-  };
-
-  const nicknameReset = () => {
-    dispatch(changeNickname(""));
-    dispatch(setNicknameCheckNull());
-  };
 
   const onClickModal = () => {
     if (isModal === true) {
@@ -293,13 +208,6 @@ const Step1Page = () => {
     } else {
       setIsModal(true);
     }
-  };
-
-  const onClickCheckNickname = () => {
-    if (!nickname) {
-      return;
-    }
-    dispatch(nicknameCheck(nickname));
   };
 
   return (
@@ -319,142 +227,7 @@ const Step1Page = () => {
           <h1>Step1</h1>
           <p>간단한 프로필을 완성해주세요</p>
         </div>
-
-        <form className="profile">
-          <div className="nickname-container">
-            <div className="nickname_title">
-              <label>닉네임</label>
-              {nicknameCheckError === false && (
-                <span className="success">사용 가능한 닉네임이에요.</span>
-              )}
-              {nicknameCheckError === true && (
-                <span className="error">동일한 닉네임이 있어요.</span>
-              )}
-            </div>
-
-            <div className="nickname-input">
-              {nicknameCheckError === false ? (
-                <input
-                  type="text"
-                  value={nickname}
-                  onChange={onChangeNickname}
-                  disabled
-                />
-              ) : (
-                <input
-                  type="text"
-                  className={nicknameCheckError === true ? "error" : ""}
-                  value={nickname}
-                  onChange={onChangeNickname}
-                />
-              )}
-
-              {nicknameCheckError === null && (
-                <span onClick={onClickCheckNickname}>중복확인</span>
-              )}
-              {nicknameCheckError === false && (
-                <img src={NicknameCheck} alt="닉네임중복체크기능" />
-              )}
-              {nicknameCheckError === true && (
-                <>
-                  <span className="error" onClick={onClickCheckNickname}>
-                    중복확인
-                  </span>
-                  <img
-                    src={NicknameError}
-                    onClick={nicknameReset}
-                    alt="닉네임중복체크기능"
-                  />
-                </>
-              )}
-            </div>
-          </div>
-
-          <div className="birthday-gender">
-            <div className="birthday-gender-title">
-              <label>생년월일/성별</label>
-              {birthdayError && <span>생년월일을 다시 입력해주세요!</span>}
-            </div>
-            <div className="birthday-input-gender-checkbox">
-              <div className="birthday-input-container">
-                <input
-                  type="text"
-                  value={birthday}
-                  placeholder="YYYY MM DD"
-                  pattern="[0-9]+"
-                  maxLength={8}
-                  onChange={onChangeBirthday}
-                />
-              </div>
-              <div className="gender-checkbox-container">
-                <div className="male" onClick={() => checkGender("MALE")}>
-                  {gender === "MALE" ? (
-                    <img src={GenderGreenCheckButton} alt="체크박스" />
-                  ) : (
-                    <img src={GenderGrayCheckButton} alt="체크박스" />
-                  )}
-                  <span>남</span>
-                </div>
-                <div className="female" onClick={() => checkGender("FEMALE")}>
-                  {gender === "FEMALE" ? (
-                    <img src={GenderGreenCheckButton} alt="체크박스" />
-                  ) : (
-                    <img src={GenderGrayCheckButton} alt="체크박스" />
-                  )}
-                  <span>여</span>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="location-select">
-            <label>거주지역</label>
-            <div className="location-select-container">
-              <div className="location-si">
-                <select onChange={onChangeCity}>
-                  <option value="서울">서울</option>
-                  <option value="인천">인천</option>
-                  <option value="대전">대전</option>
-                  <option value="대구">대구</option>
-                  <option value="부산">부산</option>
-                </select>
-              </div>
-              <div className="location-gu">
-                <select onChange={onChangeDistrict} defaultValue="강동구">
-                  <option value="강동구">강동구</option>
-                  <option value="송파구">송파구</option>
-                  <option value="용산구">용산구</option>
-                  <option value="은평구">은평구</option>
-                  <option value="동작구">동작구</option>
-                </select>
-              </div>
-            </div>
-          </div>
-          <button
-            disabled={
-              nickname &&
-              nicknameCheckError === false &&
-              birthday &&
-              birthdayError === false &&
-              gender
-                ? false
-                : true
-            }
-            type="submit"
-            className={
-              nickname &&
-              nicknameCheckError === false &&
-              birthday &&
-              birthdayError === false &&
-              gender
-                ? "next-step-button next-step-button-selected"
-                : "next-step-button"
-            }
-            onClick={() => navigate("/step2")}
-          >
-            다음
-          </button>
-        </form>
+        <ProfileForm />
       </Step1PageBlock>
       {isModal && <Modal onClickModal={onClickModal} />}
     </>
