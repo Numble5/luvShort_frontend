adef3aeb3e4391c311d80183973fd49b11afb09d
urther <alswlkku@ajou.ac.kr>
2022-05-09 01:03:42 +0900

    design : #101 edit cateogry color

--- src/utils/interestColor.js
+++ src/utils/interestColor.js
@@ -1,8 +1,11 @@
 export const CategoryBackgroundColor = {
-  요리: "#ffc9d0",
+  여행: "#c4d9fa",
+  스포츠: "#a3ecf0",
+  쇼핑: "#e2cbff",
+  음악: "#fbc07a",
   게임: "#f9daa0",
-  영화: "#e4d1be",
-  여행: "#e2cbff",
-  반려동물: "#e4d1b2",
   독서: "#bdebb1",
+  요리: "#ffc9d0",
+  영화시청: "#bcc3ff",
+  반료동물: "#e4b1be",
 };
