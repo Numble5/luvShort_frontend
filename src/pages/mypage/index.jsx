af3665fbc9ec9ede3e0c1fb927cf0db38c9738a1
urther <alswlkku@ajou.ac.kr>
2022-05-10 01:12:12 +0900

    fix : fix navigation bug

--- src/pages/mypage/index.jsx
+++ src/pages/mypage/index.jsx
@@ -1,11 +1,20 @@
 import Header from "@/components/header";
 import Navigator from "@/components/navigator";
+import { changeNavigator } from "@/redux/reducers/navigator";
 import React from "react";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 const MyPage = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(changeNavigator("mypage"));
+  }, []);
+
   return (
     <>
-      <Header type={"my"} />
+      <Header type={"MY"} />
       <Navigator />
     </>
   );
