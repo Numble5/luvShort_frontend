4cdef9218556f2a8b1867088e7aa0f343818534a
urther <alswlkku@ajou.ac.kr>
2022-05-10 21:55:55 +0900

    feat : #108 add profile edit page routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -6,7 +6,7 @@ import Step1Page from "@pages/step/Step1Page";
 import Main from "@pages/main";
 import Step2Page from "@pages/step/Step2Page";
 import RegisterSuccessPage from "@/pages/step/RegisterSuccessPage";
-import Interests from "@pages/Interests/noInterests";
+import Interests from "@pages/Interests";
 import LoginCallbackPage from "@pages/login/LoginCallbackPage";
 import FileUploadPage from "@pages/file-upload/FileUploadPage";
 import Detail from "@pages/detail";
@@ -14,6 +14,7 @@ import Alarm from "@pages/alarm";
 import MyPage from "@pages/mypage";
 import Profile from "@pages/profile";
 import ProfileEdit from "@pages/profileEdit";
+import AccountManage from "@pages/accountManage";
 
 const Routers = (props) => {
   return (
@@ -37,6 +38,7 @@ const Routers = (props) => {
         <Route path="/mypage" element={<MyPage />} />
         <Route path="/mypage/edit" element={<ProfileEdit />} />
         <Route path="/mypage/:id" element={<Profile />} />
+        <Route path="/manage" element={<AccountManage />} />
       </Routes>
     </>
   );
