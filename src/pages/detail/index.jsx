c3a29fa02043c2955908f5c93b2f317ece08281f
urther <alswlkku@ajou.ac.kr>
2022-05-10 15:13:42 +0900

    design : #11 add css styling in video detail page

--- src/pages/detail/index.jsx
+++ src/pages/detail/index.jsx
@@ -6,55 +6,191 @@ import { useLocation } from "react-router";
 import { FixedUploadBtn } from "@components/common/button";
 import TitlePrevHeader from "@/components/common/titlePrevHeader";
 import calDate from "@/utils/calDate";
+import { Cateogories } from "@/components/common/categories";
+import styled from "styled-components";
 
 const Detail = (props) => {
   const [, pathname] = useLocation().pathname.split("/");
-  const [videoInfo, setVideoInto] = useState(null);
+  const [videoInfo, setVideoInfo] = useState(null);
+  const [showContent, setShowContent] = useState(false);
+
+  const changeShowContent = () => {
+    setShowContent(!showContent);
+  };
+
+  const temp = {
+    video_idx: 1,
+    title: "토토와 함께하는 즐거운 하루입니다",
+    content:
+      "안녕하세요!  도토리 닯아서 도토리 39입니다.이번 영상에서는 제 귀여운 강아지 토토와 함께 지내는 일상을 찍어봤어요 :)",
+    createdDate: "2022-04-24T03:37:06",
+    videoUrl: "",
+    categories: ["여행", "스포츠", "쇼핑"],
+    uploader: {
+      nickname: "김씨",
+      profileImgUrl:
+        "https://www.epnnews.com/news/photo/202008/5216_6301_1640.jpg",
+      interest: [],
+    },
+  };
 
   const fetchData = async () => {
     try {
       const data = await request(`/api/videos/${pathname}`, "get");
-      setVideoInto(data);
+      setVideoInfo(data);
     } catch (e) {
       console.log(e);
     }
   };
 
   useEffect(() => {
-    fetchData();
+    // fetchData();
+    setVideoInfo(temp);
   }, []);
 
   return (
     <section>
       <Navigator />
       <TitlePrevHeader title={"View"} background={"black"} />
-      <div>
-        <div>
-          <div>
-            <img src={videoInfo?.uploader?.profileImgUrl} alt="프로필 이미지" />
+      <StyledDetail>
+        <div className="videoInfo">
+          <div className="videoInfo__user-info">
+            <div className="videoInfo__img-wrapper">
+              <img
+                src={videoInfo?.uploader?.profileImgUrl}
+                alt="프로필 이미지"
+              />
+            </div>
             <div>
-              <span>{videoInfo?.uploader?.nickname}</span>
-              <div>{/* 카테고리 담겨야함 */}</div>
+              <span className="videoInfo_nickname">
+                {videoInfo?.uploader?.nickname}
+              </span>
+              <ul className="videoInfo__category">
+                {videoInfo?.categories.map((category) => (
+                  <Cateogories category={category} />
+                ))}
+              </ul>
             </div>
           </div>
-          <div>{/* 하트모양 */}</div>
-        </div>
-        <div>
-          <div>
-            <div>{videoInfo?.title}</div>
-            <div>{videoInfo?.content}</div>
+          <div className="videoInfo__heart">
+            <img src="assets/heart.svg" alt="빈하트" />
           </div>
-          <div>{calDate(videoInfo?.createdDate)}</div>
         </div>
-        <div>
-          <video>
-            <source src={videoInfo?.videoUrl} />
-          </video>
+
+        <div className="videoContent">
+          <div>{videoInfo?.title}</div>
+          <div className="videoCotent__left">
+            {showContent ? (
+              <button onClick={changeShowContent}>▴</button>
+            ) : (
+              <button onClick={changeShowContent}>▼</button>
+            )}
+            <div>{calDate(videoInfo?.createdDate)}</div>
+          </div>
         </div>
-      </div>
-      <FixedUploadBtn />
+        {showContent ? (
+          <div className="videoContent_content">{videoInfo?.content}</div>
+        ) : (
+          <></>
+        )}
+      </StyledDetail>
+      <VideoWrapper>
+        <video>
+          <source src={videoInfo?.videoUrl} />
+        </video>
+      </VideoWrapper>
     </section>
   );
 };
 
 export default Detail;
+
+export const StyledDetail = styled.div`
+  background-color: #3d3d3d;
+  border-radius: 0px 0px 10px 10px;
+  padding-bottom: 20px;
+
+  .videoInfo {
+    display: flex;
+    justify-content: space-between;
+    width: 90%;
+    margin: 0 auto;
+  }
+
+  .videoInfo__user-info {
+    display: flex;
+    align-items: center;
+
+    .videoInfo_nickname {
+      font-size: 16px;
+      color: white;
+    }
+  }
+
+  .videoInfo__category {
+    margin-top: 5px;
+    li {
+      margin-right: 4px;
+    }
+  }
+
+  .videoInfo__heart {
+    img {
+      width: 45px;
+      height: 45px;
+    }
+  }
+
+  .videoInfo__img-wrapper {
+    width: 55px;
+    height: 55px;
+    background-color: red;
+    position: relative;
+    overflow: hidden;
+    border-radius: 50%;
+    border: 2px solid #f3576c;
+    margin-right: 10px;
+
+    img {
+      position: absolute;
+      top: 0;
+      left: 50%;
+      transform: translateX(-50%);
+      height: 100%;
+    }
+  }
+
+  .videoContent {
+    display: flex;
+    width: 90%;
+    margin: 20px auto;
+    justify-content: space-between;
+    align-items: center;
+
+    * {
+      color: white;
+    }
+  }
+  .videoCotent__left {
+    display: flex;
+    align-items: center;
+
+    > button {
+      font-size: 16px;
+      background-color: transparent;
+      border: none;
+      margin-right: 15px;
+    }
+  }
+
+  .videoContent_content {
+    width: 90%;
+    margin: 0 auto;
+    line-height: 1.5;
+    color: #cecece;
+  }
+`;
+
+const VideoWrapper = styled.div`
+  width: 100%;
+`;
