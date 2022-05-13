13090769da75c61a6f8cc9d1d33daf7127bf5086
yaulhae <gilga4@naver.com>
2022-05-14 01:48:25 +0900

    feat : #108 edit profile file submit

--- src/pages/profileEdit/index.jsx
+++ src/pages/profileEdit/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
@@ -9,11 +9,74 @@ import ProfileForm from "@components/step1/ProfileForm";
 import TitlePrevHeader from "@components/common/titlePrevHeader";
 import Navigator from "@components/navigator";
 import { changeNavigator } from "@redux/reducers/navigator";
+import { client } from "@/lib/api";
 
 const ProfileEdit = () => {
   const dispatch = useDispatch();
+  const [profileFile, setProfile] = useState("");
+  const [thumbnailSrc, setThumbnailSrc] = useState("");
   const user = useSelector(({ user }) => user);
+  const nickname = useSelector(({ user }) => user.nickname);
+  const birthday = useSelector(({ user }) => user.birthday);
+  const gender = useSelector(({ user }) => user.gender);
+  const city = useSelector(({ user }) => user.city);
+  const district = useSelector(({ user }) => user.district);
+  const interests = null;
+  const myIntroduce = null;
   const navigate = useNavigate();
+
+  const onClickProfileEdit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+
+    const info = {
+      nickname: nickname,
+      birthday: birthday,
+      gender: gender,
+      city: city,
+      district: district,
+      interests: interests,
+      myIntroduce: myIntroduce,
+    };
+    formData.append(profileFile, profileFile);
+    formData.append(
+      "info",
+      new Blob([JSON.stringify(info)], { type: "application/json" })
+    );
+    const config = {
+      headers: { "content-type": "multipart/form-data" },
+    };
+    try {
+      await client
+        .post("/api/videos/upload/direct", formData, config)
+        .then((res) => console.log(res));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const getProfileFile = (e) => {
+    const file = e.target.files[0];
+    const fileExt = file.name.split(".").pop();
+    if (
+      file.type !== "image/jpeg" &&
+      file.type !== "image/png" &&
+      fileExt !== "jpg" &&
+      fileExt !== "png"
+    ) {
+      alert("jpg, png 파일만 업로드 가능합니다.");
+      return;
+    }
+    setProfile(file);
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    return new Promise((resolve) => {
+      reader.onload = () => {
+        setThumbnailSrc(reader.result);
+        resolve();
+      };
+    });
+  };
 
   useEffect(() => {
     if (!user.user) {
@@ -30,15 +93,32 @@ const ProfileEdit = () => {
       <TitlePrevHeader title={"프로필 편집"} background={"white"} />
       <ProfileEditForm>
         <div className="profileEdit__img">
-          <button className="profile__img-edit">프로필 사진변경</button>
+          <div>
+            <img
+              src={thumbnailSrc ? thumbnailSrc : ""}
+              alt="프로필 사진 변경"
+            />
+          </div>
+          <label className="profile__img-edit" htmlFor="profile_img">
+            프로필 사진변경
+          </label>
+          <input
+            type="file"
+            id="profile_img"
+            onChange={(e) => getProfileFile(e)}
+          />
         </div>
 
         <InterestCategories title={"관심사"} categories={user.user.interests} />
 
-        {/* <ProfileForm /> */}
+        <ProfileForm />
         <label>한줄 소개</label>
         <input placeholder="20자 이내로 작성해주세요." />
-        <button type="submit" className="profile__save">
+        <button
+          type="submit"
+          className="profile__save"
+          onClick={(e) => onClickProfileEdit(e)}
+        >
           저장
         </button>
       </ProfileEditForm>
@@ -51,12 +131,31 @@ const ProfileEdit = () => {
 export default ProfileEdit;
 
 const ProfileEditForm = styled.form`
-  background-color: red;
   width: 90%;
   margin: 0 auto;
 
+  #profile_img {
+    position: absolute;
+    width: 0;
+    height: 0;
+    padding: 0;
+    overflow: hidden;
+    border: 0;
+  }
+
   .profileEdit__img {
     margin-bottom: 20px;
+
+    div {
+      width: 108px;
+      height: 108px;
+      border-radius: 50%;
+      overflow: hidden;
+
+      img {
+        height: auto;
+      }
+    }
   }
 
   .profile__img-edit,
