33dbd45c6a59e3dc69e012d35803158ce35df1ed
yaulhae <gilga4@naver.com>
2022-05-16 20:09:44 +0900

    fix : kakao-login-env-data

--- src/pages/login/KakaoRedirectPage.jsx
+++ src/pages/login/KakaoRedirectPage.jsx
@@ -50,7 +50,7 @@ const KakaoRedirect = () => {
       data: makeFormData({
         grant_type: "authorization_code",
         client_id: process.env.REACT_APP_KAKAO_REST_API_KEY,
-        redirect_uri: process.env.REACT_APP_REDIRECT_URI,
+        redirect_uri: KAKAO_AUTH_URL,
         code,
       }),
     }).then((res) => {
